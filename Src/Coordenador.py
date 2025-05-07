72724547ae1dec117c91a88cdfac0304815df35c
ItalloOtavio <otavio.itallo@gmail.com>
2025-05-07 17:38:04 -0300

    Class cordenador ( decisao )

--- Src/Coordenador.py
+++ Src/Coordenador.py
@@ -12,3 +12,12 @@ class Coordenador:
 
         print("\n[Coordenador] Todos os votos foram recebidos.")
         return votos
+
+    def decidir_transacao(self, votos):
+        if all(voto == "sim" for voto in votos.values()):
+            print("\n[DECISÃO FINAL] Todos votaram 'sim'. Transação será CONFIRMADA (COMMIT).")
+            return "COMMIT"
+        else:
+            print("\n[DECISÃO FINAL] Algum participante votou 'não'. Transação será CANCELADA (ABORT).")
+            return "ABORT"
+
